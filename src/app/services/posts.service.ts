4faa14d76927607ff9f710e52a3b828c37d77a87
Ghabriel <ghabriel.nunes@gmail.com>
2018-04-03 11:05:58 -0300

    Removed unnecessary methods

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -1,9 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/observable/of';
 import { Observable } from 'rxjs/Observable';
 
-import { environment } from './../../environments/environment';
 import { Post } from './../types';
 
 @Injectable()
@@ -13,26 +11,6 @@ export class PostsService {
 
     getPosts(): Observable<Post[]> {
         return this.http.get<Post[]>('http://jsonplaceholder.typicode.com/posts');
-    }
-
-    private post(path: string, body: any): Observable<any> {
-        return this.http.post(this.url(path), body);
-    }
-
-    private get(path: string): Observable<any> {
-        return this.http.get(this.url(path));
-    }
-
-    private put(path: string, body: any): Observable<any> {
-        return this.http.put(this.url(path), body);
-    }
-
-    private delete(path: string): Observable<any> {
-        return this.http.delete(this.url(path));
-    }
-
-    private url(path: string): string {
-        return environment.api.host + path;
     }
 
 }
